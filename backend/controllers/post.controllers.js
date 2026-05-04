e2866f3c04c4e0a251b8b43d2eb4fc529018d7df
Zaid <164772082+zka-fdsfo@users.noreply.github.com>
2026-05-04 20:12:11 +0530

    frontend

--- backend/controllers/post.controllers.js
+++ backend/controllers/post.controllers.js
@@ -29,4 +29,13 @@ async function createpost(req, res) {
     }
 }
 
-module.exports = { createpost };
+async function getPosts(req, res) {
+    try {
+        const posts = await Post.find().populate("author", "email");
+        res.status(200).json({ posts });
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+module.exports = { createpost, getPosts };
